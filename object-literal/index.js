b95005255eb25d32003fcd77250b7ff3072aaa73
Shubhamgupta00795 <shubamgupta00795@gmail.com>
2020-05-25 10:12:31 +0530

    Direct obejct creation is done

---
+++ object-literal/index.js
@@ -0,0 +1,7 @@
+var car = new Object();
+car.name = "Toyota";
+car.year = 2001;
+car.country = "India"
+document.write("My car name is "+car.name+ "<br>");
+document.write("My car year is "+car.year+ "<br>");
+document.write("My car country is "+car.country+ "<br>");
